c21c7f2daa6dd83b74fbcaf91eb8bd04d19695ee
ztata <zacharyptata@gmail.com>
2021-08-28 12:33:53 -0400

    Z Tata GC unit 1 deliverable 2 version 1

---
+++ ZTataDeliverable2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ZTataDeliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            string answerDoubleCheck;
+            Console.WriteLine("Hello and welcome to chatting with bots. What would you like to say to the bot? Please enter text below to begin.");
+            userInput = Console.ReadLine();
+            userInput = userInput.ToLower();
+
+ 
+            if (userInput == "hello")
+            {
+                Console.WriteLine("Hi good to see you");
+            }
+            else if (userInput == "sup")
+            {
+                Console.WriteLine("I am good.");
+            }
+            else if (userInput == "hello there")
+            {
+                Console.WriteLine("General Kenobi");
+            }
+
+            answerDoubleCheck = userInput;
+
+            if (userInput != "bye") {
+                do
+                {
+                    userInput = Console.ReadLine();
+                    userInput = userInput.ToLower();
+
+                    if (answerDoubleCheck == userInput)
+                    {
+                        Console.WriteLine("You already said that");
+                    }
+
+                    else if (userInput == "hello")
+                    {
+                        Console.WriteLine("Hi good to see you");
+                    }
+                    else if (userInput == "sup")
+                    {
+                        Console.WriteLine("I am good.");
+                    }
+                    else if (userInput == "hello there")
+                    {
+                        Console.WriteLine("General Kenobi");
+                    }
+
+                    answerDoubleCheck = userInput;
+
+                } while (userInput != "bye");
+            }
+
+
+            Console.WriteLine("Good bye!");
+            Console.Write("Please press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
